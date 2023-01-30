73ab6ac44ddb70d5bca2fbacf54e3c272e38f1b2
MRohit1953 <74250257+MRohit1953@users.noreply.github.com>
2023-01-30 07:44:59 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AppService } from '../cart.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  items = this.AppService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+    phone: '',
+  });
+
+  constructor(
+    private AppService: AppService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.AppService.clearApp();
+    console.warn('Your data has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
